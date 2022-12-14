78e3efd9c155590e4bdac23f21a1c334726f20ca
Marko Stanimirovic <marko.stanimirovic@jobcloud.ch>
2022-12-14 14:05:38 +0100

    feat: add reactive users page without store layer

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, startWith } from 'rxjs';
 import { UsersService } from './users.service';
 import { User } from './user.model';
 
@@ -15,7 +15,7 @@ import { User } from './user.model';
     <input type="text" placeholder="Search..." [formControl]="queryControl" />
 
     <ul>
-      <li *ngFor="let user of filteredUsers">
+      <li *ngFor="let user of filteredUsers$ | async">
         {{ user.firstName + ' ' + user.lastName }}
       </li>
     </ul>
@@ -23,51 +23,52 @@ import { User } from './user.model';
     <div>
       <button
         *ngFor="let pageSize of pageSizes"
-        [class.active]="pageSize === selectedPageSize"
+        [class.active]="pageSize === (selectedPageSize$ | async)"
         (click)="onUpdateSelectedPageSize(pageSize)"
       >
         {{ pageSize }}
       </button>
     </div>
   `,
-  styles: ['.active { background-color: aqua }'],
+  styles: ['.active { background-color: aqua; }'],
 })
-export class UsersComponent implements OnInit, OnDestroy {
+export class UsersComponent implements OnInit {
   private readonly usersService = inject(UsersService);
 
   readonly pageSizes = [1, 3, 5, 10];
   readonly queryControl = new FormControl('', { nonNullable: true });
-  private querySubscription: Subscription | undefined;
-  allUsers: User[] = [];
-  filteredUsers: User[] = [];
-  selectedPageSize = 5;
+
+  readonly allUsers$ = new BehaviorSubject<User[]>([]);
+  readonly selectedPageSize$ = new BehaviorSubject(5);
+  readonly query$ = this.queryControl.valueChanges.pipe(
+    startWith(this.queryControl.value),
+    map((query) => query.trim().toLowerCase())
+  );
+  readonly filteredUsers$ = combineLatest({
+    allUsers: this.allUsers$,
+    selectedPageSize: this.selectedPageSize$,
+    query: this.query$,
+  }).pipe(
+    map(({ allUsers, selectedPageSize, query }) =>
+      filterUsers(allUsers, query, selectedPageSize)
+    )
+  );
 
   ngOnInit(): void {
-    this.usersService.getAll().subscribe((users) => {
-      this.allUsers = users;
-      this.filteredUsers = users.slice(0, this.selectedPageSize);
-    });
-
-    this.querySubscription = this.queryControl.valueChanges.subscribe(
-      (query) => {
-        query = query.trim().toLowerCase();
-        this.filteredUsers = this.allUsers
-          .filter(({ firstName }) => firstName.toLowerCase().includes(query))
-          .slice(0, this.selectedPageSize);
-      }
-    );
+    this.usersService.getAll().subscribe(this.allUsers$);
   }
 
   onUpdateSelectedPageSize(pageSize: number): void {
-    this.selectedPageSize = pageSize;
-    this.filteredUsers = this.allUsers
-      .filter(({ firstName }) =>
-        firstName.toLowerCase().includes(this.queryControl.value)
-      )
-      .slice(0, this.selectedPageSize);
+    this.selectedPageSize$.next(pageSize);
   }
+}
 
-  ngOnDestroy(): void {
-    this.querySubscription?.unsubscribe();
-  }
+function filterUsers(
+  allUsers: User[],
+  query: string,
+  pageSize: number
+): User[] {
+  return allUsers
+    .filter(({ firstName }) => firstName.toLowerCase().includes(query))
+    .slice(0, pageSize);
 }
